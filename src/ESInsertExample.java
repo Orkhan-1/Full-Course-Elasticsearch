5a9ac62ccc4b69cf04dff96a8d2835b5cc6af8b4
Orkhan <gasanov.orkhan1@gmail.com>
2025-09-09 02:12:54 -0700

    [Tutorials 2] Setup ELK

---
+++ src/ESInsertExample.java
@@ -0,0 +1,27 @@
+import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.elasticsearch._types.Refresh;
+import co.elastic.clients.elasticsearch.core.IndexResponse;
+import co.elastic.clients.transport.rest_client.RestClientTransport;
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RestClient;
+import co.elastic.clients.json.jackson.JacksonJsonpMapper;
+
+public class ESInsertExample {
+    public static void main(String[] args) throws Exception {
+        RestClient restClient = RestClient.builder(new HttpHost("localhost", 9200)).build();
+        ElasticsearchClient client = new ElasticsearchClient(
+                new RestClientTransport(restClient, new JacksonJsonpMapper()));
+
+        IndexResponse response = client.index(i -> i
+                .index("products")
+                .id("2")
+                .document(new Product("TV", 2000))
+                .refresh(Refresh.True)
+        );
+
+        System.out.println("Indexed with ID: " + response.id());
+        restClient.close();
+    }
+
+    record Product(String name, double price) {}
+}
